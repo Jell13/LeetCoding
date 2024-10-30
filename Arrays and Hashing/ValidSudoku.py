f14c306af9558e3529caf1ccbe9b13ee0ca54810
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-10-30 09:23:14 -0700

    solved isValidSudoku, and added the notes

--- Arrays and Hashing/ValidSudoku.py
+++ Arrays and Hashing/ValidSudoku.py
@@ -1,28 +1,21 @@
-from collections import *
+class Solution:
+    def isValidSudoku(self, board: List[List[int]]) -> bool:
 
-def isValidSudoku(board):
-    rows = defaultdict(set)
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
 
-    for r in range(9):
-        for c in range(9):
-            if board[r][c] == ".":
-                continue
-            if board[r][c] in rows[r]:
-                return False
-            rows[r].add(board[r][c])
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[(r//3, c//3)]):
+                    return False
+                
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
 
-    for k, v in rows.items():
-        print(f"Row: {k}, Values: {v}")
-
-
-board = [["1","2",".",".","3",".",".",".","."],
- ["4",".",".","5",".",".",".",".","."],
- [".","9","8",".",".",".",".",".","3"],
- ["5",".",".",".","6",".",".",".","4"],
- [".",".",".","8",".","3",".",".","5"],
- ["7",".",".",".","2",".",".",".","6"],
- [".",".",".",".",".",".","2",".","."],
- [".",".",".","4","1","9",".",".","8"],
- [".",".",".",".","8",".",".","7","9"]]
-
-isValidSudoku(board)
+        return True
