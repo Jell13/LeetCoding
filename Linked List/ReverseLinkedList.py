20be0e25320edaf68fc464a60291e7f42e16282c
Jason Sugiharto <jasonliesugiharto@gmail.com>
2025-01-20 11:59:41 -0800

    solved binary search questions and linked list questions

---
+++ Linked List/ReverseLinkedList.py
@@ -0,0 +1,9 @@
+def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    prev = None
+    while head:
+        nxt = head.next
+        head.next = prev
+        prev = head
+        head = nxt
+
+    return prev
